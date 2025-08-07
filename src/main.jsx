358cf820e682843b0cb3a00275750bbe9a23f9ca
ywlee0422 <ywlee0422@naver.com>
2025-08-07 16:06:09 +0900

    프로젝트 초기 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import MainPage from '../src/components/mainPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <MainPage/>
+  </StrictMode>
 )
